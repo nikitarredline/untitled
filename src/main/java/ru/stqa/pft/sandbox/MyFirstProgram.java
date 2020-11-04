7e6efb2bd6104962a2ad6f8c335db8d724066173
nikitarredline <52927806+nikitarredline@users.noreply.github.com>
2020-11-05 01:56:54 +0300

    Задание №4

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,11 @@
 package ru.stqa.pft.sandbox;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
-
+    
 }
